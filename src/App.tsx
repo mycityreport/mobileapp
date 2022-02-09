a51cfffb0dad5c7f86f555936a5a0da54b458ba8
Aruneko <aruneko99@gmail.com>
2022-02-09 15:40:13 +0900

    enable recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { MainStackNavigation } from '@screens/mainStackNavigation'
+import { RecoilRoot } from 'recoil'
 
 export const App: React.VFC = () => {
   return (
-    <NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
         <MainStackNavigation />
         <StatusBar style="auto" />
-    </NavigationContainer>
+      </NavigationContainer>
+    </RecoilRoot>
   )
 }
