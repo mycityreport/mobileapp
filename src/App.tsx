2e0ffddd6a00b6cb1322a35f45768bd683e917f4
Aruneko <aruneko99@gmail.com>
2022-02-09 15:01:07 +0900

    add navigation container

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
 export const App: React.VFC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   )
 }
 
