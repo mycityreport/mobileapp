30b81d65fd7e966e4f3c50319c8c1f446f384553
Aruneko <aruneko99@gmail.com>
2022-02-09 15:24:18 +0900

    use stack screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { MainStackScreen } from '@screens/mainStackScreen'
 
 export const App: React.VFC = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <MainStackScreen />
         <StatusBar style="auto" />
-      </View>
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
