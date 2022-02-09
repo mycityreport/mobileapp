6eaa52e38cdf156e331eb2a0d3240ad7a19f8de1
Aruneko <aruneko99@gmail.com>
2022-02-09 15:34:50 +0900

    show main screen

--- src/screens/mainStackNavigation/index.tsx
+++ src/screens/mainStackNavigation/index.tsx
@@ -1,6 +1,13 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { MainScreen } from '@screens/mainScreen'
 import React from 'react'
-import { View } from 'react-native'
+
+const Stack = createNativeStackNavigator<MainStackNavigationProps>()
 
 export const MainStackNavigation: React.VFC = () => {
-  return <View></View>
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="mainScreen">{() => <MainScreen />}</Stack.Screen>
+    </Stack.Navigator>
+  )
 }
---
+++ src/screens/mainStackNavigation/navigationProps.ts
@@ -0,0 +1,3 @@
+type MainStackNavigationProps = {
+  mainScreen: undefined
+}
