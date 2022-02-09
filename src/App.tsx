75071f64c56531a3cc38debea71b0d1660774d0b
Aruneko <aruneko99@gmail.com>
2022-02-09 14:25:04 +0900

    make src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export const App: React.VFC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
