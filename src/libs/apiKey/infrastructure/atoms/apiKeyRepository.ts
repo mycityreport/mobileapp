019aa0a2a7ca0fc8f536bfd8ef8cce1edd6b67af
Aruneko <aruneko99@gmail.com>
2022-02-10 11:36:13 +0900

    provide repo and usecases as recoil states

---
+++ src/libs/apiKey/infrastructure/atoms/apiKeyRepository.ts
@@ -0,0 +1,7 @@
+import { atom } from 'recoil'
+import { ExpoApiKeyRepository } from '../expoApiKeyRepository'
+
+export const apiKeyRepository = atom({
+  key: 'apiKeyRepository',
+  default: new ExpoApiKeyRepository(),
+})
