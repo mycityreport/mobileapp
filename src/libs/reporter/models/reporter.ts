fb8a5079a784cb3cc3fc83ac942d814641db34a5
Aruneko <aruneko99@gmail.com>
2022-02-10 17:03:49 +0900

    refactoring

--- src/libs/reporter/models/reporter.ts
+++ src/libs/reporter/models/reporter.ts
@@ -2,13 +2,10 @@ import { NickName } from './nickname'
 import { ReporterID } from './reporterID'
 
 export class Reporter {
-  private readonly _id: ReporterID
-  private readonly _nickname: NickName
-
-  constructor(id: ReporterID, nickname: NickName) {
-    this._id = id
-    this._nickname = nickname
-  }
+  constructor(
+    private readonly _id: ReporterID,
+    private readonly _nickname: NickName
+  ) {}
 
   public get id(): number {
     return this._id.value
