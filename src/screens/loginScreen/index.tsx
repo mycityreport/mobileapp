34b512e331c668f8df220ca0724ae68f9982e5f2
Aruneko <aruneko99@gmail.com>
2022-02-16 16:36:41 +0900

    define blank login and sign up screens

---
+++ src/screens/loginScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const LoginScreen: React.VFC = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  )
+}
