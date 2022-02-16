36dc6782e55610b76b393feddeacd6f294f3ab3d
Aruneko <aruneko99@gmail.com>
2022-02-16 23:53:12 +0900

    remove api key in logout button

--- src/screens/homeScreen/components/logoutButton.tsx
+++ src/screens/homeScreen/components/logoutButton.tsx
@@ -1,3 +1,4 @@
+import { apiKeyState } from '@libs/apiKey/infrastructure/react/atoms/apiKey'
 import { deleteApiKeyInstance } from '@libs/apiKey/infrastructure/react/atoms/apiKeyUseCases'
 import { reporterState } from '@libs/reporter/infrastructure/react/atoms/reporter'
 import React, { useCallback } from 'react'
@@ -7,11 +8,13 @@ import { useRecoilValue, useSetRecoilState } from 'recoil'
 export const LogoutButton: React.VFC = () => {
   const deleteApiKey = useRecoilValue(deleteApiKeyInstance)
   const setReporter = useSetRecoilState(reporterState)
+  const setApiKey = useSetRecoilState(apiKeyState)
 
   const logout = useCallback(async () => {
     await deleteApiKey()
     setReporter(undefined)
-  }, [deleteApiKey, setReporter])
+    setApiKey(undefined)
+  }, [deleteApiKey, setApiKey, setReporter])
 
   const onPress = useCallback(async () => {
     await logout()
