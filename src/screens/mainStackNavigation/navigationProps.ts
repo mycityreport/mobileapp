3c5b65760fa9825c3be5939166241ced9ca928a0
Aruneko <aruneko99@gmail.com>
2022-02-16 16:37:23 +0900

    define new navigation props

--- src/screens/mainStackNavigation/navigationProps.ts
+++ src/screens/mainStackNavigation/navigationProps.ts
@@ -1,3 +1,19 @@
-export type MainStackNavigationProps = {
-  mainScreen: undefined
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+
+export type MainStackParamList = {
+  MainScreen: undefined
+  OnboardingScreen: undefined
+  LoginScreen: undefined
+  SignUpScreen: undefined
+  HomeScreen: undefined
 }
+
+export type LoginScreenNavigationProp = NativeStackNavigationProp<
+  MainStackParamList,
+  'LoginScreen'
+>
+
+export type SignUpScreenNavigationProp = NativeStackNavigationProp<
+  MainStackParamList,
+  'SignUpScreen'
+>
