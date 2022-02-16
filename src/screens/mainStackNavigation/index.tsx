e1499d06e016264fb1e3ce087ec6db4358bdadb7
Aruneko <aruneko99@gmail.com>
2022-02-16 16:38:11 +0900

    make stack for screens

--- src/screens/mainStackNavigation/index.tsx
+++ src/screens/mainStackNavigation/index.tsx
@@ -1,14 +1,22 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { LoginScreen } from '@screens/loginScreen'
 import { MainScreen } from '@screens/mainScreen'
+import { OnboardingScreen } from '@screens/onboardingScreen'
+import { SignUpScreen } from '@screens/signUpScreen'
 import React from 'react'
-import { MainStackNavigationProps } from './navigationProps'
+import { MainStackParamList } from './navigationProps'
 
-const Stack = createNativeStackNavigator<MainStackNavigationProps>()
+const Stack = createNativeStackNavigator<MainStackParamList>()
 
 export const MainStackNavigation: React.VFC = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="mainScreen">{() => <MainScreen />}</Stack.Screen>
+    <Stack.Navigator initialRouteName="OnboardingScreen">
+      <Stack.Screen name="MainScreen">{() => <MainScreen />}</Stack.Screen>
+      <Stack.Screen name="OnboardingScreen" options={{ headerShown: false }}>
+        {() => <OnboardingScreen />}
+      </Stack.Screen>
+      <Stack.Screen name="LoginScreen">{() => <LoginScreen />}</Stack.Screen>
+      <Stack.Screen name="SignUpScreen">{() => <SignUpScreen />}</Stack.Screen>
     </Stack.Navigator>
   )
 }
