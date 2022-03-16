a0e246b4db71435b55106e6f88bfbc1462cfc3ad
Aruneko <aruneko99@gmail.com>
2022-03-17 01:12:10 +0900

    use selector

--- src/screens/initScreen/hooks/useIsAuthenticated.ts
+++ src/screens/initScreen/hooks/useIsAuthenticated.ts
@@ -1,21 +1,15 @@
 import { apiKeyState } from '@libs/apiKey/infrastructure/react/atoms/apiKey'
 import { reporterState } from '@libs/reporter/infrastructure/react/atoms/reporter'
-import { useEffect, useState } from 'react'
-import { useRecoilValue } from 'recoil'
+import { selector } from 'recoil'
 
-export function useIsAuthenticated() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+export const isAuthenticated = selector<boolean>({
+  key: 'IsAuthenticated',
+  get: ({ get }) => {
+    const apiKey = get(apiKeyState)
+    const reporter = get(reporterState)
 
-  const apiKey = useRecoilValue(apiKeyState)
-  const reporter = useRecoilValue(reporterState)
-
-  useEffect(() => {
     const hasApiKey = apiKey !== undefined
     const hasReporter = reporter !== undefined
-    const state = hasApiKey && hasReporter
-
-    setIsAuthenticated(state)
-  }, [apiKey, reporter])
-
-  return { isAuthenticated }
-}
+    return hasApiKey && hasReporter
+  },
+})
