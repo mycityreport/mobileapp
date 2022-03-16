f191bcec996c0a977e871f7f47acb5fa975e4fd3
Aruneko <aruneko99@gmail.com>
2022-03-17 01:10:37 +0900

    use async default value

--- src/libs/apiKey/infrastructure/react/atoms/apiKey.ts
+++ src/libs/apiKey/infrastructure/react/atoms/apiKey.ts
@@ -1,7 +1,34 @@
-import { atom } from 'recoil'
+import { ApiKey } from '@libs/apiKey/models/apiKey'
+import { atom, selector } from 'recoil'
 import { ApiKeyObj } from '../object/apiKey'
+import { getApiKeyInstance } from './apiKeyUseCases'
 
-export const apiKeyState = atom<ApiKeyObj | undefined>({
-  key: 'ApiKey',
+const apiKeyAtom = atom<ApiKeyObj | undefined>({
+  key: 'ApiKeyAtom',
   default: undefined,
+})
+
+export const apiKeyState = selector<ApiKeyObj | undefined>({
+  key: 'ApiKey',
+  get: async ({ get }) => {
+    const storedApiKey = get(apiKeyAtom)
+    if (storedApiKey !== undefined) {
+      return storedApiKey
+    }
+    try {
+      const getApiKey = get(getApiKeyInstance)
+      const apiKeyDTO = await getApiKey()
+      const apiKey = ApiKey.of(apiKeyDTO.value)
+      return apiKey
+    } catch (e) {
+      if (e instanceof Error) {
+        // TODO: This message should sent into Sentry
+        console.log(`API key is not saved: ${e.message}`)
+      }
+      return undefined
+    }
+  },
+  set: ({ set }, newApiKey) => {
+    set(apiKeyAtom, newApiKey)
+  },
 })
--- src/libs/reporter/infrastructure/react/atoms/reporter.ts
+++ src/libs/reporter/infrastructure/react/atoms/reporter.ts
@@ -1,7 +1,40 @@
-import { atom } from 'recoil'
+import { apiKeyState } from '@libs/apiKey/infrastructure/react/atoms/apiKey'
+import { ApiKey } from '@libs/apiKey/models/apiKey'
+import { atom, selector } from 'recoil'
 import { ReporterObj } from '../object/reporter'
+import { fetchReporterWithApiKeyInstance } from './fetchReporterWithApiKeyUseCase'
 
-export const reporterState = atom<ReporterObj | undefined>({
-  key: 'Reporter',
+const reporterAtom = atom<ReporterObj | undefined>({
+  key: 'ReporterAtom',
   default: undefined,
+})
+
+export const reporterState = selector<ReporterObj | undefined>({
+  key: 'Reporter',
+  get: async ({ get }) => {
+    const storedReporter = get(reporterAtom)
+    if (storedReporter !== undefined) {
+      return storedReporter
+    }
+    const fetchReporterWithApiKey = get(fetchReporterWithApiKeyInstance)
+    const apiKeyObj = get(apiKeyState)
+    if (apiKeyObj !== undefined) {
+      try {
+        const apiKey = ApiKey.of(apiKeyObj.value)
+        const reporterDTO = await fetchReporterWithApiKey(apiKey)
+        return reporterDTO
+      } catch (e) {
+        if (e instanceof Error) {
+          // TODO: This message should sent into Sentry
+          console.log(`Failed to auth with stored api key: ${e.message}`)
+        }
+        return undefined
+      }
+    } else {
+      return undefined
+    }
+  },
+  set: ({ set }, newReporter) => {
+    set(reporterAtom, newReporter)
+  },
 })
